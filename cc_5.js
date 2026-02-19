b213f672cf433ce9ab924088f40112db5b59d892
andrew <andrewbai@usf.edu>
2026-02-19 14:15:33 -0500

    finished step 4 , 5 , 6 ,7

--- cc_5.js
+++ cc_5.js
@@ -10,20 +10,33 @@ function calculateBasePay(rate, hours){
     return rate * Math.min(hours,40)
 }
 // Step 4 
-function calculateOvertimePay(rate, hours)
-{    if (hours => 40)
-        return rate * hours * 1.5;
+function calculateOvertimePay(rate, hours){
+    if (hours > 40)
+        return (hours - 40) * rate * 1.5 
+return 0;
 }
+
 // Step 5 
 function calculateTaxes(grossPay) {
 return grossPay * 0.15
 }
 
 function processPayroll(employee) {
-const name = name;
+const name = employee.name;
 const basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
 const overtimePay = calculateOvertimePay (employees.hourlyRate, employees.hoursWorked)
 const grossPay = basePay + overtimePay
 const netpay = grossPay - calculateTaxes(grossPay)
-}
 
+return {
+name: employee.name,
+basePay: basePay, 
+overtimePay: overtimePay,
+netpay: netpay,
+grossPay: grossPay
+}
+}
+employees.forEach(function(employee) {
+console.log(processPayroll(employee));
+
+})
