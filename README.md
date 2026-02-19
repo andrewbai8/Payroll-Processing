29ca0670c5e014e63d5e4c6e7bfdc7ba39b72baa
andrew <andrewbai@usf.edu>
2026-02-19 13:45:49 -0500

    finished step 2, and step 3

---
+++ README.md
@@ -0,0 +1,4 @@
+# Coding Challenge 05
+Payroll Processing Toolkit: Automating Repetitive Calculations
+
+Made By Andrew Bai
---
+++ cc_5.js
@@ -0,0 +1,13 @@
+// Step 2 Created Employee Payroll
+
+const employees = [
+    { name: "Andrew Bai", hourlyRate: 100, hoursWorked: 35 },
+    { name: "Billy Bob", hourlyRate: 25, hoursWorked: 55 },
+    { name: "Lily", hourlyRate: 35, hoursWorked: 45 }
+]
+// Step 3 Now needing the functions for the loop
+function calculateBasePay(rate, hours){
+    return rate * Math.min(hours,40)
+}
+
+
